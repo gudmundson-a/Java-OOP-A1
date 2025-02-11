639290b4b652cdcda942d3af96d23e785835c629
nyx <antongudmundson@yahoo.com>
2025-02-11 20:34:29 +0100

    Comments should be added.

--- src/Raise.java
+++ src/Raise.java
@@ -2,6 +2,15 @@ public class Raise {
     public static int raiseOneCounter;
     public static int raiseHalfCounter;
 
+    /*
+    Här har vi en rekursiv metod som beräknar
+    x^k. Vi har en double x som motsvara basen och
+    en int k som motsvarar exponenten. X ska alltså
+    multipliceras med sig självt k antalet gånger.
+
+    Vi drar bort 1 från k varje gång vi anropar
+    funktionen på nytt för att närma oss basfallet.
+     */
     public static double recRaiseOne(double x, int k) {
         raiseOneCounter++;
         if (k == 0) {
@@ -11,6 +20,21 @@ public class Raise {
         }
     }
 
+    /*
+    Här är tanken att skapa en metod som liknar recRaiseOne.
+    Den ska alltså vara rekursiv beräkna x^k igen, men den
+    här gången vill vi att k ska halveras för varje anrop
+    enligt ekvation 4.
+
+    Till att börja med ska man kolla ifall värdet på k==0,
+    isåfall ska 1 returneras.
+    Är det så att värdet på k är jämt ska x^k/2 * x^k/2 köras.
+    Är värdet på k udda ska x*x^k/2 * x^k/2 köras. (fortfarande
+    enligt ekvation 4).
+
+    Man ska även se till att skapa en lokal variabel för x^k/2
+    och använda den för att få fram slutresultatet.
+     */
     public static double recRaiseHalf(double x, int k){
         raiseHalfCounter++;
             // base case
@@ -18,18 +42,25 @@ public class Raise {
           return 1.0;
            }
         else if (k % 2 == 0){
-
             double halfPower = recRaiseHalf(x, k/2);
             return halfPower * halfPower;
            }
-
         else {
                double halfPower = recRaiseHalf(x, k/2);
                return x * halfPower * halfPower;
         }
     }
 
-    public static void testRaiseMethods(){
+    /*
+    I task 9 ska vi skapa en göra ett test där man beräknar
+    x^k med hjälp av båda metoderna. Man ska skriva ut resultaten
+    samt antalet rekursiva anrop som behövdes för att nå basfallet.
+
+    Detta ska appliceras med en loop som räknar från 1-15. Alltså
+    vi vill ha ett värde på k som går från 1-15 och kör båda
+    metoderna var för sig.
+     */
+    public static void task9(){
         double x = 1.5;
         for (int k = 1; k <= 15; k++){
             raiseHalfCounter = 0;
@@ -51,7 +82,7 @@ public class Raise {
 //        System.out.println(recRaiseHalf(2, 10));
 //        System.out.println(recRaiseHalf(5, 0));
 
-//        testRaiseMethods();
+//        task9();
 
 //          for (int i = 0; i <= 10000; i++){
 //              raiseOneCounter = 0;
--- src/lifeLength.java
+++ src/lifeLength.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class lifeLength {
 
+    /*
+    Skapa en metod som beräknar f1(a0), om talet är 0 ska vi returnera 1.
+    Är talet ett heltal ska det delas med två och returneras.
+    Är talet udda ska det gångras med 3, sedan ska 1 adderas och slutligen returneras.
+     */
     public static int f1(int a0) {
         if (a0 == 0) {
             return 1;
@@ -12,6 +17,13 @@ public class lifeLength {
         }
     }
 
+    /*
+    Ett gäng metoder som anropar f1 flertalet gånger.
+    Ska användas för att visa vilket tal vi befinner oss på
+    efter ett visst antal anrop av funktionen.
+
+    f2 ska köra f1 två gånger, f4 ska köra f2 2 gånger osv osv.
+     */
     public static int f2(int a0){
         return f1(f1(a0));
     }
@@ -32,7 +44,10 @@ public class lifeLength {
         return f16(f16(a0));
     }
 
-    //i = 0 gives correct number of steps while i < n.
+    /*
+    En loop som hjälper oss beräkna vilket värde vi befinner oss på efter n
+    antal steg.
+     */
     public static int iterateF(int a0, int n){
         for(int i = 0; i < n; i++){
             a0 = f1(a0);
@@ -40,6 +55,10 @@ public class lifeLength {
             return a0;
     }
 
+    /*
+    En loop som ska hålla koll på hur många gånger man behöver köra
+    f(a0) innan man tar sig till 1.
+     */
     public static int iterLifeLength(int a0){
         int c = 0;
         while (a0 != 1){
@@ -49,13 +68,23 @@ public class lifeLength {
         return c;
     }
 
+    /*
+    Skapa en metod som ska hjälpa oss skriva ut livslängden när man kör
+    funktionen f1(a0) med ett specifikt värde i form av en string.
+     */
     public static String intToString(int i, int len){
         return "The life length of " + i + " is "  + len + ".";
     }
 
-    /* Du behöver ha 1 + recLifeLength(f1(a0)) för att närma dig basfallet, +1 gör så att
-    antalet steg det tar för att nå basfallet räknas med.
-   */
+    /*
+    Här ska vi göra en rekursiv variant där målet är att beräkna
+    hur många gånger man behöver köra f1(a0) för att ta oss till basfallet 1.
+
+    Vi behöver ha med 1 + för att den ska räkna hur många gånger
+    den behöver köra refLifeLength(f1(a0)) för att nå basfallet.
+    Metoden lägger ihop antalet efter "påvägen tillbaka" efter
+    den kallat sig själv nog många gånger för att nå basfallet.
+     */
     public static int recLifeLength(int a0){
         if (a0 == 1){
             return 0;
@@ -64,6 +93,16 @@ public class lifeLength {
         }
     }
 
+    /*
+    I task 1 ska vi ta input från användaren med en scanner.
+    Användaren ska få välja ett heltal som ska köras genom
+    f1 metoden och sedan returnera dess värde.
+
+    Metoden behöver ha koll på eventuella fel vid input.
+    Endast heltal ska accepteras som giltigt input.
+    Användaren ska kunna avsluta programmet med genom
+    att ge input == 0.
+     */
     public static void task1(){
     Scanner sc = new Scanner(System.in);
     int a0;
@@ -86,6 +125,18 @@ public class lifeLength {
      }
     }
 
+    /*
+    I task2 ska vi igen ta input från användaren med en scanner.
+    Vi behöver kunna hantera fel vid input annat än ett heltal.
+    Användaren ska igen kunna avsluta metoden med hjälpa av
+    input == 0.
+
+    Vi ska ta input som användaren ger och anropa ett antal
+    metoder som beräknar f1 flertalet gånger. Metoderna ska
+    ta input som parameter och returnera värdet på a0 efter
+    det körts via metoderna. Detta ska skrivas ut till användaren
+    med hjälp av en sträng.
+     */
     public static void task2() {
         Scanner sc = new Scanner(System.in);
         int a0;
@@ -107,6 +158,21 @@ public class lifeLength {
         }
     }
 
+    /*
+    I task tre ska vi igen använda en scanner för att ta
+    input från användaren. Vi behöver igen kunna hantera
+    fel ifall användaren ger input som ej är ett heltal
+    Användaren ska igen kunna avsluta med hjälp av input
+    == 0.
+
+    Här ska vi göra en metod som tar input från användaren
+    kallar på iterateF med två input från användaren (a0, n) som
+    parametrar, dessa två parametrar ska vara ett värde
+    som man vill köra genom f1 och den andra parametern
+    ska vara hur många gånger man vill köra f1 på det första
+    värdet(a0). Metoden ska returnera det nuvarande värdet på (a0)
+    efter det körts genom metoden n antalet gånger.
+     */
     public static void task3(){
 
         Scanner sc = new Scanner(System.in);
@@ -136,6 +202,15 @@ public class lifeLength {
         }
     }
 
+    /*
+    I task 4 behöver vi skapa en metod som loopar igenom
+    från 1-15 och för varje steg så tar den värdet på
+    countern (i) och kör iterLifeLength med countern som
+    parameter. Sen behöver vi kalla på intToString
+    och skriva ut vad vi har för värde på (i) samt
+    hur många steg som krävs för värdet på countern
+    att nå 1.
+     */
     public static void task4() {
         for (int i = 1; i <= 15; i++) {
             int len = iterLifeLength(i);
@@ -143,14 +218,21 @@ public class lifeLength {
         }
     }
 
-        public static void task6() {
-            for (int i = 1; i <= 15; i++) {
-                System.out.println("iterLifeLength: " + iterLifeLength(i) + " recLifeLength: " + recLifeLength(i));
-            }
+    /*
+    En metod som ska loopa igenom från 1-15, metoderna iterLifeLength
+    och recLifeLength ska ta värdet från countern i som paramater och sedan
+    returnera hur många steg det tar för metoden att nå 1 med de givna parameterna.
+
+    Den kommer alltså göra detta med värden från 1-15.
+     */
+    public static void task6() {
+        for (int i = 1; i <= 15; i++) {
+            System.out.println("iterLifeLength: " + iterLifeLength(i) + " recLifeLength: " + recLifeLength(i));
         }
+    }
 
     public static void main(String[] args) {
-        int t = 3;
+        int t = 2;
 
         switch(t){
             case 1: task1(); break;
